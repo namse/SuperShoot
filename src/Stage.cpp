21b1d92f621d27fa1f2125ffa1e8d50bcb7fc452
skatpgusskat <skatpgusskat@users.noreply.github.com>
2014-06-18 18:34:01 +0900

    add game logic : 1. next stage, 2. request new monster, 3. finish game

--- src/Stage.cpp
+++ src/Stage.cpp
@@ -1,10 +1,14 @@
 #include "DXUT.h"
 #include "Stage.h"
 #include "xmlParser.h"
+#include "event_manager.h"
 
-Stage::Stage(void):stageInfo_(NULL)
+Stage::Stage(void):stageInfo_(NULL),totalStageCount_(-1),killedMonsterInThisStage_(-1),nowStage_(-1),
+	appearedMonsterCount(-1)
 {
 	LoadStageInfo();
+
+	g_event_manager->AddEventListener( event::EVENT_MONSTER_DIE, this);
 }
 
 
@@ -13,21 +17,100 @@ Stage::~Stage(void)
 	delete[] stageInfo_;
 }
 
+
+void Stage::Notify(event::EventHeader& event)
+{
+	switch (event.event_type_)//(EventTypes)
+	{
+	case event::EVENT_NO:
+		break;
+	case event::EVENT_MONSTER_DIE:
+		{
+			event::MonsterDieEvent& monster_die_event = (event::MonsterDieEvent &)(event);
+
+			killedMonsterInThisStage_ ++;
+
+			if( stageInfo_[nowStage_].monsterCount >= killedMonsterInThisStage_ )
+			{
+				nextStage();
+			}
+		}break;
+	}
+}
+
+void Stage::nextStage()
+{
+	if(nowStage_ == -1)
+	{
+		nowStage_ = 1;
+	}
+	else
+		nowStage_++;
+
+	if(nowStage_ > totalStageCount_)
+	{
+		finishGame();
+	}
+	else
+	{
+		appearedMonsterCount = killedMonsterInThisStage_ = 0;
+		regenerateDelay = calculateNextRegenTime();
+
+		event::StageStartEvent next_event;
+		next_event.stageNumber = nowStage_;
+		next_event.event_sender_p_ = this;
+		g_event_manager->Notify(next_event);
+	}
+}
+
+
+void Stage::finishGame()
+{
+	nowStage_ = -1;
+	event::FinishGameEvent event;
+	event.event_sender_p_ = this;
+	g_event_manager->Notify(event);
+}
+
+
+void Stage::Update(float dTime)
+{
+	if(nowStage_ > 0 && nowStage_ <= totalStageCount_)
+	{
+
+		if( appearedMonsterCount < stageInfo_[nowStage_].monsterCount )
+		{
+			regenerateDelay -= dTime;
+
+			if(regenerateDelay <= 0.f)
+			{
+				appearedMonsterCount++;
+
+				event::RequestNewMonsterEvent event;
+				event.event_sender_p_ = this;
+				g_event_manager->Notify(event);
+
+				regenerateDelay = calculateNextRegenTime();
+			}
+		}
+
+	}
+}
 void Stage::LoadStageInfo()
 {
 	XMLNode xMainNode=XMLNode::openFileHelper(L"stage.xml",L"PMML");
 
 	// this prints "<Condor>":
 	XMLNode xNode=xMainNode.getChildNode(L"Stage");
-	
+
 	totalStageCount_ = xNode.nChildNode(L"StageInfo");
-	
+
 	if(stageInfo_ != NULL)
 	{
 		delete[] stageInfo_;
 	}
 	stageInfo_ = new StageInfo[totalStageCount_];
-	
+
 	for ( int i = 0 ; i < totalStageCount_; i++)
 	{
 		stageInfo_[i].stageNumber = _wtoi(xNode.getChildNode(L"StageInfo", i).getAttribute(L"stageNumber"));
@@ -36,4 +119,9 @@ void Stage::LoadStageInfo()
 	}
 
 	return;
+}
+
+float Stage::calculateNextRegenTime()
+{
+	return stageInfo_[nowStage_].averageRegenerateTime * (1.f + 0.5f * ((float) rand() / (RAND_MAX)) );
 }
--- src/Stage.h
+++ src/Stage.h
@@ -1,22 +1,34 @@
 #pragma once
-
+#include "event_listener.h"
 struct StageInfo{
 	int stageNumber;
 	int monsterCount;
 	int averageRegenerateTime;
 };
-class Stage
+class Stage : public EventListener
 {
 public:
 	Stage(void);
 	~Stage(void);
 
+	void Notify(event::EventHeader& event);
+	void Update(float dTime);
+
 private:
 	void LoadStageInfo();
-
+	void nextStage();
+	void finishGame();
+	
+	float calculateNextRegenTime();
 
 	int totalStageCount_;
 	StageInfo *stageInfo_;
+
+	int appearedMonsterCount;
+	int killedMonsterInThisStage_;
+	int nowStage_;
+
+	float regenerateDelay;
 
 };
 
